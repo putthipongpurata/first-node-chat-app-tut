cb3211a30473a5d91fd1027bfe6c33d39479e605
unknown <putthipongpurata@gmail.com>
2017-08-13 12:50:27 +0700

    Add form

---
+++ public/js/index.js
@@ -0,0 +1,29 @@
+var socket = io();
+
+socket.on('connect', function () {
+  console.log('Connected to server');
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+  console.log('newMessage', message);
+  var li = jQuery('<li></li>');
+  li.text(`${message.from}: ${message.text}`);
+
+  jQuery('#messages').append(li);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+  e.preventDefault();
+  var $form = $(this);
+  socket.emit('createMessage', {
+    from: 'User',
+    text: jQuery('[name=message]').val()
+  }, function () {
+
+  	$form.children('input').val('');
+  });
+});
